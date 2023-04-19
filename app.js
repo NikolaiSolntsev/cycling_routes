1999024aa43d8df696d9e54dab28481fd4b3768a
NikolaiSolntsev <sonnennik@gmail.com>
2023-04-19 19:32:00 +0300

    data base done

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 require('@babel/register');
-
-
-
 const { PORT } = process.env;
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
---
+++ db/migrations/20230419152505-create-trip.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Trips', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      map: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      length: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      trip_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      creator: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Trips');
+  }
+};

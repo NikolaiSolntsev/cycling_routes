34b55aac753ccc328e4bb9ea09ed74ca2b8c1414
Ilia <gerasimbek3@gmail.com>
2023-04-20 21:25:40 +0300

    CSS SVEDENIA

--- components/InfoPage.jsx
+++ components/InfoPage.jsx
@@ -4,13 +4,13 @@ const Layout = require('./Layout');
 function InfoPage({ title, trip }) {
   return (
     <Layout title={title}>
-      <div className="card-info" style={{ width: '18rem' }}>
+      <div className="cardorado">
         <img src={trip.map} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{trip.trip_name}</h5>
-          <p className='card-text'>{trip.trip_length}</p>
-          <p className='card-text'>{trip.description}</p>
-          <p className='card-text'>{trip.creator}</p>
+          <h5 className="card-titleso">{trip.trip_name}</h5>
+          <p className="card-texto">{trip.trip_length}</p>
+          <p className="card-texto">{trip.description}</p>
+          <p className="card-texto">{trip.creator}</p>
         </div>
       </div>
     </Layout>

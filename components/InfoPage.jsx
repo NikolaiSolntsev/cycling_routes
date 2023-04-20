87089125243402a58f131c019840648b3e0b15ba
Konstantin Mosenkov <mosenkovk@mail.ru>
2023-04-20 17:18:44 +0300

    components

--- components/InfoPage.jsx
+++ components/InfoPage.jsx
@@ -7,8 +7,9 @@ function InfoPage({ title, trip }) {
       <div className="card" style={{ width: '18rem' }}>
         <img src={trip.map} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{trip.length}</h5>
-          <p className='card-text'>{trip.trip_name}</p>
+          <h5 className="card-title">{trip.trip_name}</h5>
+          <p className='card-text'>{trip.trip_length}</p>
+          <p className='card-text'>{trip.creator}</p>
         </div>
       </div>
     </Layout>
